70e70d84f1c0e40cb65c8f69a220aa2a10246e00
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-02-13 17:13:29 +0000

    added menu options and completed storage functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from rsa import sign
 from auth import login, signUp
-from storage import upload
+from storage import upload, download, view
 
 startUp = input("Welcome to the firebase test app. Do you want to (L)ogin or (S)ign Up? ").upper()
 
@@ -9,11 +9,30 @@ if startUp == "L":
 elif startUp == "S":
     signUp()
 
-print("What would you like to do?")
-mode = input("(U)pload a file, (Q)uit?: ").upper()
+# Main Menu
+while True:
+    print("What would you like to do?")
+    mode = input("(A)ccount management, (S)torage, (D)atabase, (Q)uit?: ").upper()
 
-if mode == "U":
-    upload()
-elif mode =="Q":
-    pass
+    if mode == "A":
+        print("Not built out yet...")
 
+    if mode == "S":
+
+        # Storage Menu
+        while True:
+            storage_mode = input("(V)iew files in browser, (U)pload file, (D)ownload, (Q)uit to main menu: ").upper()
+            if storage_mode == "V":
+                view()
+            if storage_mode == "U":
+                upload()
+            if storage_mode == "D":
+                download()
+            elif storage_mode == "Q":
+                break
+    
+    if mode == "D":
+        print("Database functionality not built out yet")
+
+    elif mode =="Q":
+        break
--- storage.py
+++ storage.py
@@ -1,13 +1,21 @@
 import pyrebase
 from config import firebaseConfig
+import os
 
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 
 storage = firebase.storage()
 
-# Uploading files
 
+# Viewing web folder
+def view():
+    projectID = os.environ.get('PROJECT_ID')
+    storage_url = os.environ.get('STORAGE_BUCKET')
+    url = f"https://console.firebase.google.com/u/0/project/{projectID}/storage/{storage_url}/files"
+    print("👇 The storage folder url is 👇 " + "\n",  url)
+
+# Uploading files
 def upload():
 
     with open('.token') as file:
@@ -15,5 +23,15 @@ def upload():
 
     filename = input("Enter the name of the file you wish to upload: ")
     storage.child(filename).put(filename, token)
-
     
+    print("File uploaded successfully.")
+    print(storage.child(filename).get_url(None))
+
+
+# Downloading files
+def download():
+
+    filename = input("Enter the name of the file you wish to download: ")
+    localname = input("What do you wish to save the file as?: ")
+    storage.child(filename).download("", localname)
+    print("File downloaded successfully")
